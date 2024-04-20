3a5474588373cc032c8de3ec12413279d83e4b1f
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2024-04-20 11:35:38 +0200

    R_DrawColumnFlat is never called with count <= 0

--- i_mac.c
+++ i_mac.c
@@ -422,10 +422,6 @@ void R_DrawColumnFlat(int16_t texture, const draw_column_vars_t *dcvars)
 {
 	int16_t count = (dcvars->yh - dcvars->yl) + 1;
 
-	// Zero length, column does not exceed a pixel.
-	if (count <= 0)
-		return;
-
 	const uint8_t color1 = texture;
 	const uint8_t color2 = (color1 << 4 | color1 >> 4);
 	const uint8_t colort = color1 + color2;
