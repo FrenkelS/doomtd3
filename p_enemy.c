a5c3d8500d0cf6c5d072d88edd0b6b12ecd19286
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2024-05-12 12:23:50 +0200

    remove includes

--- p_enemy.c
+++ p_enemy.c
@@ -35,18 +35,8 @@
  *
  *-----------------------------------------------------------------------------*/
 
-#include "d_player.h"
 #include "m_random.h"
-#include "r_main.h"
-#include "p_maputl.h"
-#include "p_map.h"
-#include "p_setup.h"
-#include "p_spec.h"
-#include "sounds.h"
 #include "p_inter.h"
-#include "g_game.h"
-#include "p_enemy.h"
-#include "p_tick.h"
 
 #include "globdata.h"
 
@@ -291,7 +281,7 @@ static boolean P_TryWalk(mobj_t __far* actor)
 // Most of P_NewChaseDir(), except for what
 // determines the new direction to take
 //
-#include "i_system.h"
+
 static void P_DoNewChaseDir(mobj_t __far* actor, fixed_t deltax, fixed_t deltay)
 {
   int16_t xdir, ydir, tdir;
