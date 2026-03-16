7ed9e7e5e589a8e70929cf44db887b2f195fc66f
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2026-03-16 19:02:52 +0100

    undef __far

--- compiler.h
+++ compiler.h
@@ -1,6 +1,6 @@
 //
 //
-// Copyright (C) 2023-2025 Frenkel Smeijers
+// Copyright (C) 2023-2026 Frenkel Smeijers
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -51,6 +51,7 @@ int32_t labs(int32_t v);
 typedef uint32_t segment_t;
 #define SIZE_OF_SEGMENT_T 4
 
+#undef  __far
 #define __far
 
 #define _fmemcpy	memcpy
